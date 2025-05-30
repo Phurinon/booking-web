c89301c28f040e8346e321426733d07d4fbe133a
Phurinon <folk.5834@gmail.com>
2025-05-30 16:10:14 +0700

    เพิ่ม api สร้าง profile และ place

--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
 import express from 'express'
-import morgan from 'morgan'
-import winston from 'winston'
 import dotenv from 'dotenv'
 dotenv.config()
 import logger from './logger.js'
+import cors from 'cors'
+
+// Import routes
+import userRoutes from './routes/user/index.js'
+import handleError from './middleware/error.js'
 
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
 
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+// Routing
+app.use('/api/user', userRoutes)
+
+// Error handling middleware
+app.use(handleError)
 
 
 app.listen(port, () => {
-    logger.info(`Server is running on port ${port}`)
+    logger.info(`Server is running at http://localhost:${port}`)
 })
