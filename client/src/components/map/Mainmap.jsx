f9f1fc8f18293e516eb6d9b00a8c306ad4b1f9d8
Phurinon <folk.5834@gmail.com>
2025-05-30 16:24:36 +0700

    แก้ให้ส่งค่า lat, lng ไปกับ form

--- client/src/components/map/Mainmap.jsx
+++ client/src/components/map/Mainmap.jsx
@@ -9,45 +9,50 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useState } from "react";
+import { Form, Input } from 'antd'
 
-function LocationMarker({ position, setPosition, setValue }) {
+function LocationMarker({ position, setPosition, form }) {
   const map = useMapEvents({
     click(e) {
       setPosition(e.latlng);
       map.flyTo(e.latlng, map.getZoom());
-      if (setValue) {
-        setValue("lat", e.latlng.lat);
-        setValue("lng", e.latlng.lng);
+      if (form) {
+        form.setFieldsValue({
+          lat: e.latlng.lat,
+          lng: e.latlng.lng,
+        });
       }
     },
   });
 
   return position === null ? null : (
     <Marker position={position}>
-      <Popup>You are here</Popup>
+      <Popup>คุณอยู่ที่นี่</Popup>
     </Marker>
   );
 }
 
-function Mainmap({ register, Location, setValue }) {
+function Mainmap({ form, Location }) {
   const DEFAULT_LOCATION = [13, 100];
   const [position, setPosition] = useState(null);
+
   return (
     <div>
-      {register && (
-        <>
-          <input hidden {...register("lat")} />
-          <input hidden {...register("lng")} />
-        </>
+      <h1 className="font-bold mt-4">ปักหมุดของคุณ</h1>
+      {position && (
+        <p className="text-sm text-gray-500">
+          Coordinates: {position.lat.toFixed(6)}, {position.lng.toFixed(6)}
+        </p>
       )}
-      <h1 className="font-bold mt-4">Where Are You?</h1>
-      {
-        position && (
-            <p className="text-sm text-gray-500">
-                Coordinates : {position.lat.toFixed(6)}, {position.lng.toFixed(6)}
-            </p>
-        )
-      }
+
+      {/* ซ่อน input เก็บค่าพิกัดใน Form */}
+      <Form.Item name="lat" noStyle>
+        <Input type="hidden" />
+      </Form.Item>
+      <Form.Item name="lng" noStyle>
+        <Input type="hidden" />
+      </Form.Item>
+
       <MapContainer
         center={Location || DEFAULT_LOCATION}
         zoom={7}
@@ -59,14 +64,11 @@ function Mainmap({ register, Location, setValue }) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        <LocationMarker
-          position={position}
-          setPosition={setPosition}
-          setValue={setValue}
-        />
+        <LocationMarker position={position} setPosition={setPosition} form={form} />
       </MapContainer>
     </div>
   );
 }
+
 
 export default Mainmap;
